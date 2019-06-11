f1c25ec103d6f9f784a18624573b375d7fc96a42
nakatomo3.0501@gmail.com <nakatomo3.0501@gmail.com>
2019-06-11 17:38:56 +0900

    プレイヤーの移動処理完成

---
+++ PL22_2Project/Assets/Scripts/Player.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+	public static Player player;
+	private Transform thisTransform;
+
+	/// <summary>
+	/// 通常時、左右移動までに必要な時間
+	/// </summary>
+	private float normalHorizontalSpeed = 0.3f;
+
+	/// <summary>
+	/// 通常時、上下移動までに必要な時間
+	/// </summary>
+	private float normalVerticalSpeed = 0.3f;
+
+
+	/// <summary>
+	/// ハサミ時、左右移動までに必要な時間
+	/// </summary>
+	private float cuttingHorizontalSpeed = 1f;
+
+	/// <summary>
+	/// ハサミ時、上下移動までに必要な時間
+	/// </summary>
+	private float cuttingVerticalSpeed = 1f;
+
+	private float moveTimer;
+	private const float moveRange = 0.5f;
+
+	private bool isCuttingMode;
+
+	public Slider moveSlider;
+
+	// Start is called before the first frame update
+	void Start() {
+		thisTransform = transform;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		float speed = 9999999;
+
+		if (Input.GetKey(KeyCode.Space)) {
+			isCuttingMode = true;
+		} else {
+			isCuttingMode = false;
+		}
+
+		if (isCuttingMode == false) {
+			if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+				moveTimer += Time.deltaTime;
+				speed = normalHorizontalSpeed;
+				if(moveTimer > normalHorizontalSpeed && SystemManager.RIGHT_MAX >= thisTransform.position.x + moveRange) {
+					thisTransform.position += Vector3.right * moveRange;
+					moveTimer = 0;
+				}
+			} else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+				moveTimer += Time.deltaTime;
+				speed = normalHorizontalSpeed;
+				if(moveTimer > normalHorizontalSpeed && SystemManager.LEFT_MAX <= thisTransform.position.x - moveRange) {
+					thisTransform.position += Vector3.left * moveRange;
+					moveTimer = 0;
+				}
+			} else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
+				moveTimer += Time.deltaTime;
+				speed = normalVerticalSpeed;
+				if (moveTimer > normalVerticalSpeed && SystemManager.UP_MAX >= thisTransform.position.z + moveRange) {
+					thisTransform.position += Vector3.forward * moveRange;
+					moveTimer = 0;
+				}
+			} else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+				moveTimer += Time.deltaTime;
+				speed = normalVerticalSpeed;
+				if (moveTimer > normalVerticalSpeed && SystemManager.DOWN_MAX <= thisTransform.position.z - moveRange) {
+					thisTransform.position += Vector3.back * moveRange;
+					moveTimer = 0;
+				}
+			}
+		} else {
+			if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+				moveTimer += Time.deltaTime;
+				speed = cuttingHorizontalSpeed;
+				if (moveTimer > cuttingHorizontalSpeed && SystemManager.RIGHT_MAX >= thisTransform.position.x + moveRange) {
+					thisTransform.position += Vector3.right * moveRange;
+					moveTimer = 0;
+				}
+			} else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+				moveTimer += Time.deltaTime;
+				speed = cuttingHorizontalSpeed;
+				if (moveTimer > cuttingHorizontalSpeed && SystemManager.LEFT_MAX <= thisTransform.position.x - moveRange) {
+					thisTransform.position += Vector3.left * moveRange;
+					moveTimer = 0;
+				}
+			} else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
+				moveTimer += Time.deltaTime;
+				speed = cuttingVerticalSpeed;
+				if (moveTimer > cuttingVerticalSpeed && SystemManager.UP_MAX >= thisTransform.position.z + moveRange) {
+					thisTransform.position += Vector3.forward * moveRange;
+					moveTimer = 0;
+				}
+			} else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+				moveTimer += Time.deltaTime;
+				speed = cuttingVerticalSpeed;
+				if (moveTimer > cuttingVerticalSpeed && SystemManager.DOWN_MAX <= thisTransform.position.z - moveRange) {
+					thisTransform.position += Vector3.back * moveRange;
+					moveTimer = 0;
+				}
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) {
+			moveTimer = 0;
+		}
+
+		moveSlider.value = moveTimer / speed;
+	}
+}
---
+++ PL22_2Project/Assets/Scripts/SystemManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemManager : MonoBehaviour {
+
+	public static SystemManager instance;
+
+	public static float LEFT_MAX = -5;
+	public static float RIGHT_MAX = 5;
+	public static float UP_MAX = 5;
+	public static float DOWN_MAX = -5;
+
+	/// <summary>
+	/// 敵の出現時間のリスト
+	/// </summary>
+	List<float> enemyTimer;
+
+	/// <summary>
+	/// 敵の出現場所のリスト
+	/// </summary>
+	List<Vector3> enemyPosition;
+
+	/// <summary>
+	/// 敵のタイプ
+	/// </summary>
+	List<EnemyType> enemyType;
+
+	enum EnemyType {
+		NORMAL = 0,
+		last
+	};
+
+	public GameObject[] enemyObjects;
+
+	private void Awake() {
+		instance = this;
+	}
+
+	// Start is called before the first frame update
+	void Start() {
+		enemyObjects = new GameObject[(int)EnemyType.last];
+		ReadCSV();
+		CreateEnemy();
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	private void ReadCSV() {
+
+	}
+
+	private void CreateEnemy() {
+		
+	}
+}
